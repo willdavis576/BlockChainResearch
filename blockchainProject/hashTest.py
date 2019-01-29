a9de4e636846ab35e1f96eb199ff8be29b00527b
Will Davis <william.davis@live.co.uk>
2019-01-29 22:50:21 +0000

    Inclusion of ROS and first communication with a listener from a block creation

---
+++ blockchainProject/hashTest.py
@@ -0,0 +1,12 @@
+import hashlib
+
+def encrypt_string(hash_string):
+    sha_signature = \
+        hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+
+hash_string = 'hello1'
+
+sha_signature = encrypt_string(hash_string)
+
+print(sha_signature)
