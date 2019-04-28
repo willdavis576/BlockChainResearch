8dfa0ea3a40386c388fe51be331e46f027522a95
willdavis576 <william.davis@live.co.uk>
2019-04-28 16:37:07 +0100

    Created a script which allows a customer to create an order for products using their unique licence key. The script sends the key to a licence server which varifies it and allows the customer to carry out their order.

--- 00blockChain_ws/src/blockChainPack_/src/scripts/Unused Currently/testTCP 1.py
+++ 00blockChain_ws/src/blockChainPack_/src/scripts/Unused Currently/testTCP 1.py
@@ -6,12 +6,43 @@ import hashlib, sys, random, rospy, threading, time, socket
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('172.21.0.90', 2000))
-    s.sendall('444;#RequestID=0;MClass=101;MNo=2;ErrorState=0;#PNo=211;#Aux1Int=1\r')
-    data = s.recv(1024)
+    key = raw_input("What is your key?")
+    
+    
+
+    s.connect(('172.21.4.153', 4000))
+    s.sendall(key)
+    data = s.recv(7)
     s.close()
-    print(data)
+    
+    if data == 'success':
+        var2 = raw_input("Please provide the details or your order: ProductNumber, Quantity, CustomerNumber: ")
+	params = var2.split(',')  
+
+        try:
+            productNumber = params[0]
+            quantity = params[1]
+            customerNumber = params[2]
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(('172.21.0.90', 2000))
+            sock.sendall('444;#RequestID=0;MClass=101;MNo=2;ErrorState=0;#PNo=' + productNumber + ';#CNo=' + customerNumber + ';#Aux1Int=' + quantity + ';\r')
+            data = sock.recv(1024)
+            sock.close()
+            print("Your payment has been accepted and your order has been placed")
+
+        except:
+            print("Invailed order request")
+
+        
+    if data == 'failed':
+	print("Your key was incorrect")
+    
 
 
 if __name__ == '__main__':
     main()
+
+
+
+# THIS IS THE KEY => b4718ea7acc2e60199286959bffb58a78bb123ad6eece513123f7fc3b65149f5
